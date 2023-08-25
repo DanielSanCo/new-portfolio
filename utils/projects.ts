48780e22e59024651d821fdcf8e0bdeda0059b5e
Daniel <96082772+DanielSanCo@users.noreply.github.com>
2023-08-25 14:53:19 -0300

    version 2.2

--- utils/projects.ts
+++ utils/projects.ts
@@ -7,24 +7,22 @@ const projetoList = [
         date: '24/08/2023',
         link: '/',
         github: 'https://github.com/DanielSanCo/new-portfolio',
-        txt1: 'Projeto do meu portfolio criado com o FRAMEWORK: Next.js, usei boa parte das minhas habilidades a fim de apresentar um porfolio com um design agradavel para o usuario.',
+        txt1: 'Projeto do meu portfolio criado com o FRAMEWORK: Next.js, utilizei todas as minhas habilidades no Front End a fim de apresentar um porfolio com um design intuitivo e agradavel ao usuario.',
         txt2: '',
         txt3: '',
         linkedin: '',
         categoria: 'Portfolio',
-        tec: [{
-            name: 'Next.js',
-            img: './linguagens/next.png'
-        }, {
-            name: 'Javascript',
-            img: './linguagens/javascript.png'
-        }, {
-            name: 'CSS3',
-            img: './linguagens/css.png'
-        }, {
-            name: 'HTML5',
-            img: './linguagens/html.png'
-        }]
+        tec: [
+            {
+                name: 'Next.js',
+                img: './linguagens/next.png'
+            }, {
+                name: 'CSS3',
+                img: './linguagens/css.png'
+            }, {
+                name: 'HTML5',
+                img: './linguagens/html.png'
+            }]
     },
 
     {
@@ -40,19 +38,17 @@ const projetoList = [
         txt3: '',
         linkedin: 'https://www.linkedin.com/posts/daniel-santos-corr%C3%AAa-007979217_netflix-reactjs-github-activity-6943556580231548929-4M5o?utm_source=share&utm_medium=member_desktop',
         categoria: 'Site',
-        tec: [{
-            name: 'React.js',
-            img: './linguagens/react.png'
-        }, {
-            name: 'Javascript',
-            img: './linguagens/javascript.png'
-        }, {
-            name: 'CSS3',
-            img: './linguagens/css.png'
-        }, {
-            name: 'HTML5',
-            img: './linguagens/html.png'
-        }
+        tec: [
+            {
+                name: 'React.js',
+                img: './linguagens/react.png'
+            }, {
+                name: 'CSS3',
+                img: './linguagens/css.png'
+            }, {
+                name: 'HTML5',
+                img: './linguagens/html.png'
+            }
         ]
     },
 
@@ -63,25 +59,49 @@ const projetoList = [
         name: 'Jogo da Memoria',
         date: '07/05/2022',
         link: '',
-        github: 'https://github.com/danielCorrea321/netflix-clone/tree/main',
-        txt1: 'Clone da Netflix criado com o FRAMEWORK: React.js, Consumindo API da netflix',
+        github: 'https://github.com/danielCorrea321/react-memoria',
+        txt1: 'Jogo da memoria feito com o auxilio do curso da B7Web, projeto feito com React.js.',
         txt2: '',
         txt3: '',
-        linkedin: 'https://www.linkedin.com/posts/daniel-santos-corr%C3%AAa-007979217_netflix-reactjs-github-activity-6943556580231548929-4M5o?utm_source=share&utm_medium=member_desktop',
+        linkedin: 'https://www.linkedin.com/posts/daniel-santos-corr%C3%AAa-007979217_jogo-da-mem%C3%B3ria-no-meu-github-httpslnkdin-activity-6932429266781425665-bl9l?utm_source=share&utm_medium=member_desktop',
         categoria: 'Jogo',
-        tec: [{
-            name: 'React.js',
-            img: './linguagens/react.png'
-        }, {
-            name: 'Javascript',
-            img: './linguagens/javascript.png'
-        }, {
-            name: 'CSS3',
-            img: './linguagens/css.png'
-        }, {
-            name: 'HTML5',
-            img: './linguagens/html.png'
-        }
+        tec: [
+            {
+                name: 'React.js',
+                img: './linguagens/react.png'
+            }, {
+                name: 'CSS3',
+                img: './linguagens/css.png'
+            }, {
+                name: 'HTML5',
+                img: './linguagens/html.png'
+            }
+        ]
+    },
+    {
+        img: './projetos/snakegame.png',
+        imgPc: './projetos/snakegame.png',
+        //imgPc: 'https://anniebombanie.com/assets/work-personal-date-night.png',
+        name: 'Snake Game',
+        date: '25/08/2023',
+        link: '',
+        github: 'https://github.com/DanielSanCo/snake-game',
+        txt1: 'Projeto interessante que construi através de video aulas.',
+        txt2: 'Pude, além de desenvolver mais minhas habilidades em javascript, me aprofundar em tecnologias já conhecidas como a tag canvas do HTML, aprendendo conceitos novos como a pixelização das divs das tags. Manipulação de Array afim de que ela responda a comandos do teclado de maneira automatica e dinamica.',
+        txt3: '',
+        linkedin: 'https://www.linkedin.com/posts/daniel-santos-corr%C3%AAa-007979217_github-danielsancosnake-game-activity-7100897363396898819-MU39?utm_source=share&utm_medium=member_desktop',
+        categoria: 'Jogo',
+        tec: [
+            {
+                name: 'Javascript',
+                img: './linguagens/javascript.png'
+            }, {
+                name: 'CSS3',
+                img: './linguagens/css.png'
+            }, {
+                name: 'HTML5',
+                img: './linguagens/html.png'
+            },
         ]
     },
     {
@@ -93,29 +113,27 @@ const projetoList = [
         link: 'https://generatedu.vercel.app/',
         github: 'https://github.com/DanielSanCo/proj-generatEdu',
         txt1: 'Projeto integrador do bootcamp da Generation Brasil em parceria com a Prefeitura do Rio de Janeiro.',
-        txt2: 'Projeto integrador do bootcamp da Generation Brasil em parceria com a Prefeitura do Rio de Janeiro.',
-        txt3: 'Projeto integrador do bootcamp da Generation Brasil em parceria com a Prefeitura do Rio de Janeiro. ',
+        txt2: 'Utilizamos as tecnologias React.js na parte de Front End, e Nest.js na parte do Back End.',
+        txt3: 'Foi um otimo desafio e uma experiencia enriquecedora onde eu e minha equipe de desenvolvedores pudemos aprender com nossos erros e acertos. Contribui com o Front End estrutrando boa parte do visual do site, estruturamos o banco de dados realizando o deploy no render e hospedamos o site através do vercel. ',
         linkedin: 'https://www.linkedin.com/feed/update/urn:li:activity:7027261318122418176/',
         categoria: 'Blog',
-        tec: [{
-            name: 'React.js',
-            img: './linguagens/react.png'
-        }, {
-            name: 'Javascript',
-            img: './linguagens/javascript.png'
-        }, {
-            name: 'CSS3',
-            img: './linguagens/css.png'
-        }, {
-            name: 'HTML5',
-            img: './linguagens/html.png'
-        }, {
-            name: 'Nest.js',
-            img: './linguagens/nest.png'
-        }, {
-            name: 'MySQL',
-            img: './linguagens/mysql.png'
-        },
+        tec: [
+            {
+                name: 'React.js',
+                img: './linguagens/react.png'
+            }, {
+                name: 'CSS3',
+                img: './linguagens/css.png'
+            }, {
+                name: 'HTML5',
+                img: './linguagens/html.png'
+            }, {
+                name: 'Nest.js',
+                img: './linguagens/nest.png'
+            }, {
+                name: 'MySQL',
+                img: './linguagens/mysql.png'
+            },
         ]
     },
 ]
